c65130e7c9e57144be97d6b111ad57cc6bba914a
ancevanaga <avanaga68@gmail.com>
2025-02-18 10:11:53 +0000

    dsfs

---
+++ FV.py
@@ -0,0 +1,23 @@
+teikums = input("Ievadi tekstu: ")
+vards = teikums.split()
+simboli = [vards for vards in vardi if len(vards) == 3] 
+for vards in vardi:
+    if len(vards)==3:
+        print("IR 3 SIMBOLI")
+        break
+print("NAV 3 SIMBOLI")
+#2.uzd
+
+for vards in vards:
+   if simboli:
+        print(vards)
+    else:
+print("NAV TADU VARDU")
+#3.uzd
+for i in range(len(vards)):
+    if vards[i]%2==0:
+    print(i)
+#4uzd
+
+    
+
