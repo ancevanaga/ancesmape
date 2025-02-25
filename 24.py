af3cdff5c32537cfefe8a1ad9a28230097a6e133
ancevanaga <avanaga68@gmail.com>
2025-02-25 10:01:20 +0000

    jfnjdfb

--- 24.py
+++ 24.py
@@ -1,5 +1,16 @@
 teikums = input()
 saraksts = teikums.split()
+pedejais = saraksts[-1]
 saraksts.insert(2, "Sveiks!")
 saraksts.sort(reverse=True)
 print(saraksts)
+print(saraksts.index("Sveiks!"))
+if "abc" in saraksts:
+    print("JA")
+else:
+    print("NE")
+if saraksts[-1]==saraksts[3]:
+    print("JA")
+else:
+    print("NE")
+
